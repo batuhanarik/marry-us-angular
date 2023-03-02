1f2549c7f9e07426201be0994fe74cf42e97cc39
Batuhan <batuhanarik123@gmail.com>
2023-03-02 16:40:16 +0300

    some improvements developped

---
+++ src/app/components/wedding-place/search-weddingplace/search-weddingplace.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-weddingplace',
+  templateUrl: './search-weddingplace.component.html',
+  styleUrls: ['./search-weddingplace.component.scss'],
+  standalone:true,
+  
+})
+export class SearchWeddingplaceComponent {
+
+}
--- src/app/components/wedding-place/weddingplaces/weddingplaces.component.ts
+++ src/app/components/wedding-place/weddingplaces/weddingplaces.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SearchWeddingplaceComponent } from '../search-weddingplace/search-weddingplace.component';
 
 @Component({
   selector: 'app-weddingplaces',
   templateUrl: './weddingplaces.component.html',
   styleUrls: ['./weddingplaces.component.scss'],
   standalone:true,
+  imports: [CommonModule, RouterModule, SearchWeddingplaceComponent],
 })
 export class WeddingplacesComponent {
 
